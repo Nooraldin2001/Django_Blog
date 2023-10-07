c3fb537ec710cbf69e1029ba6728af8f1cf417ed
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-07 11:15:27 +0300

    desgin post model p1

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+'''
+    - title
+    - author
+    - content
+    - img
+    - publish_date
+    - tags
+'''
+
+'''
+    - fields it choose the best field to use 
+    - html widget
+    - valdations
+'''
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=50000)
+    publish_date = models.DateTimeField()
+
+
+    
