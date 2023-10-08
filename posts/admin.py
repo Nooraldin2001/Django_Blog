886dfa25218941f3f7e6f0d33f2c009287422624
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-08 15:00:11 +0300

    adding admin customization filter

--- posts/admin.py
+++ posts/admin.py
@@ -6,6 +6,7 @@ from.models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'publish_date']
     search_fields = ['title', 'content']
+    list_filter = ['publish_date', 'author']
 
 
 
