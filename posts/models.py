efd7798ab845afd12f3c50861420962f58003c42
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-08 14:56:14 +0300

    adding author to posts

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 # Create your models here.
 '''
     - title
@@ -17,6 +18,7 @@ from taggit.managers import TaggableManager
 '''
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author')
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=50000)
     publish_date = models.DateTimeField()
