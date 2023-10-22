8cc006737a79303521bf1f578fdac1229496f5dc
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-22 07:56:27 +0300

    adding blog them front end

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 from django.utils import timezone
+import readtime
 # Create your models here.
 '''
     - title
@@ -28,6 +29,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_readtime(self):
+      result = readtime.of_text(self.content)
+      return result.text 
 
 
     
